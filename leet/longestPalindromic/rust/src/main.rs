5a15b7e2a86b4415ecbce4f0dd5360cd5d617d3f
kshammer <kshammerschmidt@gmail.com>
2022-09-17 16:37:45 -0700

    Leet code answer

--- leet/longestPalindromic/rust/src/main.rs
+++ leet/longestPalindromic/rust/src/main.rs
@@ -2,39 +2,18 @@ struct Solution;
 
 impl Solution {
     pub fn longest_palindrome(s: String) -> String {
-        let chars: Vec<char> = s.chars().collect::<Vec<_>>();
-        return Self::helper(chars); 
-    }
-    fn helper(chars: Vec<char>) -> String {
-        let mid_point:i32 = (chars.len() / 2) as i32;
-        let is_odd = chars.len() % 2 == 1;
-        let mut left:i32;
-        let mut right:i32;
-        if is_odd {
-            left = mid_point - 1; 
-            right = mid_point + 1; 
-        } else {
-            left = mid_point -1; 
-            right = mid_point;     
+        fn is_palindrome(s: &[u8]) ->bool {
+            s.iter().zip(s.iter().rev()).all(|(l, r)| l == r)
         }
-        loop {
-            let left_val = match chars.get(left as usize) {
-                Some(x) => *x, 
-                None => break,
-            };
-            let right_val = match chars.get(right as usize) {
-                Some(x) => *x, 
-                None => break, 
-            }; 
-            if left_val != right_val{
-                break; 
-            }else {
-                left = left - 1;
-                right = right + 1; 
+
+        for size in (1..=s.len()).rev() {
+            match s.as_bytes().windows(size).find(|substr| is_palindrome(substr)){
+                Some(pal) => return String::from_utf8(pal.to_vec()).unwrap(),
+                None => continue,
             }
         }
-    
-        return chars[(left + 1) as usize..right as usize].into_iter().collect();
+        // None 
+        String::from("")
     }
 }
 
@@ -46,12 +25,35 @@ mod test {
     #[test]
     fn test_babad() {
         let answer = Solution::longest_palindrome("babad".to_string());
-        assert_eq!(answer, "aba");
+        assert_eq!(answer, "bab");
     }
 
     #[test]
     fn test_cbbd() {
         let answer = Solution::longest_palindrome("cbbd".to_string());
+        assert_eq!(answer, "bb");
+    }
+
+    #[test]
+    fn test_ac() {
+        let answer = Solution::longest_palindrome("ac".to_string());
+        assert_eq!(answer, "a");
+    }
+    #[test]
+    fn test_bb() {
+        let answer = Solution::longest_palindrome("bb".to_string());
+        assert_eq!(answer, "bb");
+    }
+
+    #[test]
+    fn test_a() {
+        let answer = Solution::longest_palindrome("a".to_string());
+        assert_eq!(answer, "a");
+    }
+
+    #[test]
+    fn test_abb() {
+        let answer = Solution::longest_palindrome("abb".to_string());
         assert_eq!(answer, "bb");
     }
 }
