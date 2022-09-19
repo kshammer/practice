b6fed82757fe22a4fa42833a1fc7affe23a73b0c
kshammer <kshammerschmidt@gmail.com>
2022-09-18 19:53:35 -0700

    real

--- leet/longestPalindromic/rust/src/main.rs
+++ leet/longestPalindromic/rust/src/main.rs
@@ -2,21 +2,55 @@ struct Solution;
 
 impl Solution {
     pub fn longest_palindrome(s: String) -> String {
-        fn is_palindrome(s: &[u8]) ->bool {
-            s.iter().zip(s.iter().rev()).all(|(l, r)| l == r)
-        }
+        let chars: Vec<char> = s.chars().collect::<Vec<_>>();
+        let longest = Self::helper(&chars); 
+        let mid_point:i32 = (chars.len() / 2) as i32; 
 
-        for size in (1..=s.len()).rev() {
-            match s.as_bytes().windows(size).find(|substr| is_palindrome(substr)){
-                Some(pal) => return String::from_utf8(pal.to_vec()).unwrap(),
-                None => continue,
+        return Self::helper(&chars); 
+    }
+    fn helper(chars: &Vec<char>) -> String {
+        if chars.len() == 1 {
+            return chars[0].to_string();
+        }
+        else if chars.len() == 2 {
+            if chars[0] == chars[1] {
+                return chars.into_iter().collect(); 
+            } else {
+                return chars[0].to_string(); 
             }
         }
-        // None 
-        String::from("")
+
+        let mid_point:i32 = (chars.len() / 2) as i32;
+        let is_odd = chars.len() % 2 == 1;
+        let mut left:i32;
+        let mut right:i32;
+        if is_odd {
+            left = mid_point; 
+            right = mid_point; 
+        } else {
+            left = mid_point -1; 
+            right = mid_point;     
+        }
+        loop {
+            let left_val = match chars.get((left -1) as usize) {
+                Some(x) => *x, 
+                None => break,
+            };
+            let right_val = match chars.get((right + 1) as usize) {
+                Some(x) => *x, 
+                None => break, 
+            }; 
+            if left_val != right_val{
+                break; 
+            }else {
+                left = left - 1;
+                right = right + 1; 
+            }
+        }
+            
+        return chars[left as usize..=right as usize].into_iter().collect();
     }
 }
-
 #[cfg(test)]
 
 mod test {
@@ -25,7 +59,7 @@ mod test {
     #[test]
     fn test_babad() {
         let answer = Solution::longest_palindrome("babad".to_string());
-        assert_eq!(answer, "bab");
+        assert_eq!(answer, "aba");
     }
 
     #[test]
